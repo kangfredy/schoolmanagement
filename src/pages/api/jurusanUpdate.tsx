12608d6bec15ff1e23a603340551ac1217a61d9e
Akhmad Faizin <24859207+akhmadfaizin@users.noreply.github.com>
2023-05-27 15:20:52 +0700

    Jurusan API
    Jurusan API

---
+++ src/pages/api/jurusanUpdate.tsx
@@ -0,0 +1,28 @@
+import {PrismaClient} from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+      const { id, namaJurusan } = req.body;
+  
+      // Update Data
+      const updateJurusan = await prisma.jurusan.update({
+        where: {
+          id: id,
+        },
+        data: {
+            namaJurusan: namaJurusan,
+        },
+      })
+  
+      // Return a success or failed message
+      res.status(200).json({ message: 'Update successful', updateJurusan });
+    } else {
+      res.status(405).json({ message: 'Method not allowed' });
+    }
+  }
+
+
+
