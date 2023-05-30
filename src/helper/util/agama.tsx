19800cadad53b75ee439fbeae659355f57336db5
Akhmad Faizin <24859207+akhmadfaizin@users.noreply.github.com>
2023-05-30 15:56:34 +0700

    Add Moment, util agama time
    Add Moment, util agama time

---
+++ src/helper/util/agama.tsx
@@ -0,0 +1,16 @@
+const checkAgama = (agamaId: number) => {
+  switch (agamaId) {
+    case 1:
+      return 'Islam'
+    case 2:
+      return 'Kristen'
+    case 3:
+      return 'Katolik'
+    case 4:
+      return 'Hindu'
+    case 5:
+      return 'Budha'
+    case 6:
+      return 'Konghucu'
+  }
+}
