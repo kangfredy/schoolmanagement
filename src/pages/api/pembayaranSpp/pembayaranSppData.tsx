a29a87343b7514640e7b92fa493a4349ff788fa4
Akhmad Faizin <24859207+akhmadfaizin@users.noreply.github.com>
2023-06-02 18:43:15 +0700

    Add Nesterd Api di PembayaranSpp
    Add Nesterd Api di PembayaranSpp

--- src/pages/api/pembayaranSpp/pembayaranSppData.tsx
+++ src/pages/api/pembayaranSpp/pembayaranSppData.tsx
@@ -1,27 +1,34 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-  
-      // Get Data
-      const getPembayaranSpp = await prisma.pembayaranSpp.findMany({
-        where: {
-            isDeleted: false,
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    // Get Data
+    const getPembayaranSpp = await prisma.pembayaranSpp.findMany({
+      where: {
+        isDeleted: false,
+      },
+      include: {
+        siswa: {
+          include: {
+            kelas: {
+              include: {
+                jurusan: true,
+              },
+            },
           },
-        include: {
-            siswa: true
-          }
-      })
-  
-      // Return a success or failed message
-      res.status(200).json({ message: 'Get Data successful', getPembayaranSpp });
-    } else {
-      res.status(405).json({ message: 'Method not allowed' });
-    }
+        },
+      },
+    })
+
+    // Return a success or failed message
+    res.status(200).json({ message: 'Get Data successful', getPembayaranSpp })
+  } else {
+    res.status(405).json({ message: 'Method not allowed' })
   }
-
-
-
+}
