3b117cdec9bc92185a55a6cd1b59ddb820802d23
Akhmad Faizin <24859207+akhmadfaizin@users.noreply.github.com>
2023-06-09 19:33:52 +0700

    Generate Pembayaran Seragam

--- src/screens/PembayaranSpp.tsx
+++ src/screens/PembayaranSpp.tsx
@@ -109,10 +109,10 @@ export const PembayaranSpp = () => {
   const getHistoryPembayaranSppByPembayaranSppId = (id: number) => {
     historyPembayaranSppByPembayaranSppId(id)
       .then(response => {
-        console.log(
-          'ID',
-          response.data.getHistoryPembayaranSppById[0].pembayaranSppId,
-        )
+        // console.log(
+        //   'ID',
+        //   response.data.getHistoryPembayaranSppById[0].pembayaranSppId,
+        // )
         console.log('historyPembayaranSppByPembayaranSppId ', response)
         const arrayDataTemp: IHistorySpp[] = []
         const arraySelectTemp: any[] = []
