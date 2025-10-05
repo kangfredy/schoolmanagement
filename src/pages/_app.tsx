872f71809cae5559e26a37523faeb9eb3a04bd1e
Fredy Setiawan <holywarragnarok@gmail.com>
2025-10-05 23:13:30 +0800

    speedup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+        <SpeedInsights />
+            <Component {...pageProps} />
+            
+        </>
+    );
 }
