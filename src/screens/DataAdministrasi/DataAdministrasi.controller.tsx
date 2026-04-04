e23841e1de3421575f4bc8460aca654cb0d4bbb7
Fredy Setiawan <holywarragnarok@gmail.com>
2026-04-04 19:13:44 +0800

    fix build

--- src/screens/DataAdministrasi/DataAdministrasi.controller.tsx
+++ src/screens/DataAdministrasi/DataAdministrasi.controller.tsx
@@ -1,12 +1,11 @@
 import { UserMenu } from '@/screens/UserMenu'
 import { ReminderSPP } from '@/screens/ReminderSPP'
-import { ReminderSeragam } from '@/screens/ReminderSeragam'
 
 export function useDataAdministrasiController() {
   const menu = [
     { label: 'User menu', content: UserMenu },
     { label: 'Pengingat SPP', content: ReminderSPP },
-    { label: 'Pengingat Seragam', content: ReminderSeragam },
+   
   ]
   return { menu }
 }
